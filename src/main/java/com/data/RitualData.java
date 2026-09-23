3146ca0727d7fb971ea0b743339d261fd07d4c1d
domc888 <dutoitzhandre9@gmail.com>
2026-09-24 01:26:14 +0200

    Create RitualData.java

---
+++ src/main/java/com/data/RitualData.java
@@ -0,0 +1,10 @@
+package com.infuse.data;
+
+public class RitualData {
+    public String effectKey;
+    public String world;
+    public double x;
+    public double y;
+    public double z;
+    public long endTime; // epoch millis
+}
